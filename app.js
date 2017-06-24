59f281e07f8a88eb368061f75d59e63e55c0659a
Alan Richards <alanrichards86@gmail.compwdlsexitstopopengitopengit config --global user.name Alan>
2017-06-24 12:13:16 -0400

    Working on validation. Can log messages. Now trying to get message to show up on page.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const fs = require('fs');
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 var differentWords = words [Math.floor(Math.random() * words.length)];
 var newWord = differentWords.split('');
+var messages = [];
 
 
 const app = express();
@@ -29,12 +30,27 @@ app.use(session({
 }));
 
 app.get('/Guess-It', function(req, res){
-  
+  console.log(req.session);
   res.render('main', {randomWords: newWord});
 
 });
 
 app.post('/Guess-It', function(req, res){
+
+  req.checkBody('inputField', 'Please enter at the most 1 letter before hitting the "GUESS" button. ').notEmpty();
+  req.checkBody('inputField', 'Please enter only one letter before hitting the "GUESS" button. ').isLength({max: 1});
+
+  let errors = req.validationErrors();
+
+  if (errors){
+    errors.forEach(function(error) {
+      messages.push(error.msg);
+      console.log(messages);
+    });
+  }
+
+  let newVar = req.body.inputField;
+  req.session.guess = newVar;
   res.redirect('/Guess-It');
 });
 

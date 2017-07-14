529bc4b6fd9e05e9293c151e3ff4d5282fb77978
Alan Richards <alanrichards86@gmail.compwdlsexitstopopengitopengit config --global user.name Alan>
2017-07-14 08:29:19 -0400

    Got error messages to clear properly after someone guesses a non error guess.

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
 const path = require('path');
 const session = require('express-session');
+const router = require('./routes/router.js')
 const validator = require('express-validator');
-const fs = require('fs');
-//Variable for our random words.
-const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
-//Variable selection the random word.
-var differentWords = words [Math.floor(Math.random() * words.length)];
-//Variable spliting the random word.
-var newWord = differentWords.toUpperCase().split('');
+
+// const fs = require('fs');
+// //Variable for our random words.
+// const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+// //Variable selection the random word.
+// var differentWords = words [Math.floor(Math.random() * words.length)];
+// //Variable spliting the random word.
+// var newWord = differentWords.toUpperCase().split('');
 
 const app = express();
 
@@ -30,53 +32,9 @@ app.use(session({
   saveUninitialized: false
 }));
 
-app.get('/Guess-It', function(req, res){
-  console.log(req.session);
-  console.log(req.session.guess);
-  res.render('main', {mainError: req.session.messages,
-                      randomWords: newWord,
-                      userGuesses: req.session.allGuesses
-                      });
-});
+app.use('/', router);
 
-app.post('/Guess-It', function(req, res){
-  //Variable Containing error messages. Its placed here to clear it each time an error is found..
-  // ... so they don't stack on top of each other.
-  var messages = [];
-  req.checkBody('inputField', 'Please enter a letter to guess ....').isAlpha();
-  req.checkBody('inputField', 'Please enter at the most 1 letter before hitting the "GUESS" button. ').notEmpty();
-  req.checkBody('inputField', 'Please enter only one letter before hitting the "GUESS" button. ').isLength({max: 1});
-
-  let errors = req.validationErrors();
-
-  if (errors){
-    errors.forEach(function(error) {
-      messages = [];
-      messages.push(error.msg);
-      console.log(messages);
-
-    });
-  }
-
-    //Variable for what is typed in by the user.
-  let newVar = req.body.inputField.toUpperCase();
-  req.session.guess = newVar;
-
-  req.session.messages = messages;
-
-
-    if (!req.session.allGuesses) {
-      req.session.allGuesses = [];
-    }
-    req.session.allGuesses.push(newVar);
-
-  res.redirect('/Guess-It');
-});
-
-
-
-
-
+module.exports = app;
 
 
 app.listen(3000, function(req, res){
---
+++ routes/router.js
@@ -0,0 +1,66 @@
+const validate = require('../models/validate.js');
+const express = require('express');
+const router = express.Router();
+const app = require('../app.js');
+
+const fs = require('fs');
+//Variable for our random words.
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+//Variable selection the random word.
+var differentWords = words [Math.floor(Math.random() * words.length)];
+//Variable spliting the random word.
+var newWord = differentWords.toUpperCase().split('');
+
+router.get('/Guess-It', function(req, res){
+  // console.log(req.session);
+  // console.log(req.session.guess);
+  res.render('main', {mainError: req.session.messages,
+                      randomWords: newWord,
+                      userGuesses: req.session.allGuesses
+                      });
+});
+
+
+router.post('/Guess-It', function(req, res){
+  // Variable Containing error messages. Its placed here to clear it each time an error is found..
+  // ... so they don't stack on top of each other.
+  var messages = [];
+  req.checkBody('inputField', 'Please enter a letter to guess ....').isAlpha();
+  req.checkBody('inputField', 'Please enter at the most 1 letter before hitting the "GUESS" button. ').notEmpty();
+  req.checkBody('inputField', 'Please enter only one letter before hitting the "GUESS" button. ').isLength({max: 1});
+
+  let errors = req.validationErrors();
+
+  //Loggs Errors and Displays them
+  if (errors){
+    errors.forEach(function(error) {
+      messages = [];
+      messages.push(error.msg);
+      // console.log(messages);
+
+    });
+      validate.valid(req, res);
+      req.session.messages = messages;
+      res.redirect('/Guess-It');
+  }
+
+    //Variable for what is typed in by the user.
+  let newVar = req.body.inputField.toUpperCase();
+  req.session.guess = newVar;
+
+
+     if(!req.session.allGuesses) {
+      req.session.allGuesses = [];
+      req.session.messages = [];
+    }
+    req.session.allGuesses.push(newVar);
+
+  res.redirect('/Guess-It');
+  });
+
+
+
+
+
+
+module.exports = router;
